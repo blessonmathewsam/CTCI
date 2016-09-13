beb6f9ef47a7f6117cc26e62e3d9463d80f047e0
blessonm <blessonm@MSI>
2016-09-13 11:42:37 -0400

    Imported SinglyLinkedList and made code workable.

--- CTCI/src/chapter2/RecursiveKthToLast.java
+++ CTCI/src/chapter2/RecursiveKthToLast.java
@@ -1,19 +1,40 @@
 package chapter2;
 
+import chapter2.SinglyLinkedList.LinkedList;
 import chapter2.SinglyLinkedList.Node;
 
 public class RecursiveKthToLast {
 	
-	static int counter = 0;
+	
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.addNode(new int[] {1,2,3,4,5});
+		list.printNodes();
+		System.out.println("Kth value is " + findkth(list.getHead(), 2));
+	}
+	
+	public static class Counter {
+		public int value;
+		
+		public Counter(){
+			this.value = 0;
+		}
+	}
+	
+	public static int findkth(Node head, int k){
+		Counter counter = new Counter();
+		return findkth(head, k, counter).getData();
+	}
 
-	public static String findkth(Node head, int k){
-		if(head.getNext() == null)
+	public static Node findkth(Node head, int k, Counter counter){
+		if(head == null)
 			return null;
-		
-		counter = Integer.parseInt(findkth(head.getNext(), k) + 1);
-		
-		if(counter == k)
-			return head.getData();
-		return null;
+		Node n = findkth(head.getNext(), k, counter);	
+		counter.value = counter.value + 1;
+		if(counter.value == k){
+			System.out.println(head.getData());
+			return head;
+		}
+		return n;
 	}
 }
