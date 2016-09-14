eff09247a2a615c10462194bc5961eaa78bd9502
blessonm <blessonm@MSI>
2016-09-14 12:14:53 -0400

    A python implementation of a simply linked list.

---
+++ CTCI/src/chapter2/singlylinkedlist.py
@@ -0,0 +1,45 @@
+class Node(object):
+	"""docstring for Node"""
+	def __init__(self, data, next = None):
+		self.data = data
+		self.next = next
+
+class LinkedList(object):
+	"""docstring for LinkedList"""
+	
+	head = None
+	tail = None
+
+	def addNode(self, data):
+		if isinstance(data, Node):
+			self.addNode(data.data)
+			return
+		node = Node(data)
+		if self.head is None:
+			self.head = self.tail = node
+		else:
+			self.tail.next = node
+			self.tail = node
+
+	def printNodes(self):
+		node = self.head
+		if node.next is None:
+			print(node.data)
+			return
+		while node.next is not None:
+			print(str(node.data) + "->")
+			node = node.next
+		print(str(node.data))
+
+	def deleteNode(self, data):
+		delNode = Node(data)
+		node = self.head
+		if node.data == data:
+			self.head = node.next
+		while node.next is not None:
+			if node.next.data == data:
+				node.next = node.next.next
+
+			
+		
+		
